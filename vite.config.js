597fddc4c01fd9d8474699fc353a030260a7c544
João Pedro Pereira Lima <84398388+JoaoPedroSH@users.noreply.github.com>
2025-10-24 17:56:35 -0300

    Fix loadEnv function call in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), '')
+    const env = loadEnv('', process.cwd(), '')
     return {
         base: '/',
         plugins: [vue()],
