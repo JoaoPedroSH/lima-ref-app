9b78863f5811aeea18a20d13c0e2904fc82d70ed
JoaoPedroSH <joaopedro.shalom27@gmail.com>
2025-10-24 17:33:59 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
     return {
         base: '/',
         plugins: [vue()],
